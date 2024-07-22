a983e93b24033b0f08e924ac91bc195c80e6b1e2
QnK1 <kacperro03@gmail.com>
2024-07-22 21:59:10 +0200

    register page styling, color scheme tweaks

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,14 +1,24 @@
 from django.forms import ModelForm
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.forms import CharField, PasswordInput
+from django.utils.translation import gettext_lazy as _
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'email','password1', 'password2']
+        fields = ['username', 'email', 'password1', 'password2']
         labels = {
             'username' : 'Username',
             'email' : 'E-mail',
             'password1' : 'Password',
             'password2' : 'Repeat password',
         }
+    
+    def __init__(self, *args, **kwargs):
+        super(UserCreationForm, self).__init__(*args, **kwargs)
+
+        for fieldname in ['username', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
+        
+        self.fields['password2'].label = 'Repeat Password'
