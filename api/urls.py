f2de01d45e10d73eae8e04f78d6c2f7073a09309
QnK1 <kacperro03@gmail.com>
2024-08-17 17:40:32 +0200

    added option to give hearts to algs

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path("heart-alg/<str:pk>/", views.heartAlg),
+    path("unheart-alg/<str:pk>/", views.unheartAlg),
 ]
--- api/views.py
+++ api/views.py
@@ -5,13 +5,47 @@ from rest_framework.response import Response
 from algorithms.models import Algorithm
 
 
-@api_view(['POST'])
+@api_view(['PUT'])
 @permission_classes([IsAuthenticated])
 def heartAlg(request, pk):
     try:
         alg = Algorithm.objects.get(id=pk)
     except:
-        return
+        return Response({
+        "errors" : "Invalid ID.",
+    })
 
     profile = request.user.profile
     
+    if profile in alg.users_hearts.all():
+        return Response({
+            "errors" : "Alg already hearted by user.",
+        })
+    else:
+        alg.users_hearts.add(profile)
+        return Response({
+            "errors" : "None.",
+        })
+
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def unheartAlg(request, pk):
+    try:
+        alg = Algorithm.objects.get(id=pk)
+    except:
+        return Response({
+        "errors" : "Invalid ID.",
+    })
+        
+    profile = request.user.profile
+    
+    if profile in alg.users_hearts.all():
+        alg.users_hearts.remove(profile)
+        return Response({
+            "errors" : "None.",
+        })
+    else:
+        return Response({
+            "errors" : "Alg not previously hearted by user.",
+        })
