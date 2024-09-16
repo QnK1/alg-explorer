48a76ac4f5b0437f80d0f28b8a899ae6c8976950
QnK1 <kacperro03@gmail.com>
2024-09-16 21:32:31 +0200

    fixed nested 'a' tag bug

--- algorithms/static/algorithms/js/explore.js
+++ algorithms/static/algorithms/js/explore.js
@@ -14,37 +14,12 @@ function getCookie(name) {
     return cookieValue;
 }
 
-function outerHeight(el) {
-    var height = el.offsetHeight;
-    var style = getComputedStyle(el);
 
-    height += parseFloat(style.marginTop) + parseFloat(style.marginBottom);
-    return height;
-}
 
 window.addEventListener('load', () => {
-    const algContents = [...document.querySelectorAll('.alg-content')];
+    
 
-    const containerHeight = outerHeight(document.querySelector('.alg-card-content'));
-    const item1Height = outerHeight(document.querySelector('.alg-title-row'));
-    const item2Height = outerHeight(document.querySelector('.alg-description'));
-    const algContentSpace = containerHeight - item1Height - item2Height;
-
-
-    const fitAlgLines = () => {
-        let lineHeight;
-        let lineCount;
-        algContents.forEach((e) => {
-            lineHeight = parseFloat(getComputedStyle(e).lineHeight);
-            lineCount = Math.floor(algContentSpace / lineHeight);
-            
-            e.style = `height: ${lineCount * lineHeight}px`;
-            
-        });
-    };
-
-    fitAlgLines();
-    window.addEventListener('resize', fitAlgLines);
+    
     
     const heartBtns = [...document.querySelectorAll('.heart-info')];
     const savedBtns = [...document.querySelectorAll('.saved-info')];
@@ -114,6 +89,9 @@ window.addEventListener('load', () => {
 
     savedBtns.forEach((savedBtn) => {
         savedBtn.addEventListener('click', async (e) => {
+            
+                
+            
             e.preventDefault();
             e.stopPropagation();
             disableEvents();
---
+++ algorithms/static/algorithms/js/fit-alg-lines.js
@@ -0,0 +1,31 @@
+function outerHeight(el) {
+    var height = el.offsetHeight;
+    var style = getComputedStyle(el);
+
+    height += parseFloat(style.marginTop) + parseFloat(style.marginBottom);
+    return height;
+}
+
+window.addEventListener('load', () => {
+    const algContents = [...document.querySelectorAll('.alg-content')];
+    const containerHeight = outerHeight(document.querySelector('.alg-card-content'));
+    const item1Height = outerHeight(document.querySelector('.alg-title-row'));
+    const item2Height = outerHeight(document.querySelector('.alg-description'));
+    const algContentSpace = containerHeight - item1Height - item2Height;
+
+
+    const fitAlgLines = () => {
+        let lineHeight;
+        let lineCount;
+        algContents.forEach((e) => {
+            lineHeight = parseFloat(getComputedStyle(e).lineHeight);
+            lineCount = Math.floor(algContentSpace / lineHeight);
+            
+            e.style = `height: ${lineCount * lineHeight}px`;
+            
+        });
+    };
+
+    fitAlgLines();
+    window.addEventListener('resize', fitAlgLines);
+});
